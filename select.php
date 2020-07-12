99ebb0e8d79ed56a87bce2195dd03fd8a5845558
arnurkelgenbaev <galiaskar.uli@gmail.com>
2020-07-13 01:50:23 +0600

    some stuff

--- select.php
+++ select.php
@@ -17,7 +17,7 @@ if ($result->num_rows > 0) {
     echo "<table>";
     echo"<tr><td>ИИН</td><td>Имя абитуриента</td><td>Email</td><td>Человека-Природа</td><td>Человек-Человек</td>";
     echo"<td>Человек-художественный образ</td><td>Человек-знаковая техника</td><td>Человек-техника</td>";
-    while($row = $result->fetch_assoc($sql)) {
+    while($row = $result->fetch_assoc($result)) {
         echo "<tr><td>".$sql["iin"]."</td>"." "."<td>".$sql["full_name"]." ".$sql["email"]." ".
         substr($sql["result"],0,1)." ".substr($sql["result"],1,1)." ".substr($sql["result"],2,1)." ".
         substr($sql["result"],3,1)." ".substr($sql["result"],4,1)."</td></tr>";
