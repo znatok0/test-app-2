5f55dcf4500fc8597ee1e53a24e865bf2c70e4da
arnurkelgenbaev <galiaskar.uli@gmail.com>
2020-07-13 02:03:48 +0600

    some stucc

--- select.php
+++ select.php
@@ -14,15 +14,21 @@ $result = $connection->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
-    echo "<table>";
-    echo"<tr><td>ИИН</td><td>Имя абитуриента</td><td>Email</td><td>Человека-Природа</td><td>Человек-Человек</td>";
-    echo"<td>Человек-художественный образ</td><td>Человек-знаковая техника</td><td>Человек-техника</td></tr>";
+    // echo "<table>";
+    // echo"<tr><td>ИИН</td><td>Имя абитуриента</td><td>Email</td><td>Человека-Природа</td><td>Человек-Человек</td>";
+    // echo"<td>Человек-художественный образ</td><td>Человек-знаковая техника</td><td>Человек-техника</td></tr>";
+    // while($row = $result->fetch_assoc()) {
+    //     echo "<tr><td>".$row["iin"]."</td>"." "."<td>".$row["full_name"]." ".$row["email"]." ".
+    //     substr($row["result"],0,1)." ".substr($row["result"],1,1)." ".substr($row["result"],2,1)." ".
+    //     substr($row["result"],3,1)." ".substr($row["result"],4,1)."</td></tr>";
+    // }
+    // echo "</table>";
+    echo "<p>Список абитуриентов, сдавших тест</p>";
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row["iin"]."</td>"." "."<td>".$row["full_name"]." ".$row["email"]." ".
-        substr($row["result"],0,1)." ".substr($row["result"],1,1)." ".substr($row["result"],2,1)." ".
-        substr($row["result"],3,1)." ".substr($row["result"],4,1)."</td></tr>";
+        echo "<p>ИИН:".$row["iin"]." "."ФИО:".$row["full_name"]." "."Человека-Природа:".substr($row["result"],0,1)." ".
+        "Человек-Человек:".substr($row["result"],1,1)." "."Человек-художественный образ".substr($row["result"],2,1)." ".
+        "Человек-знаковая техника:".substr($row["result"],3,1)." "."Человек-техника:".substr($row["result"],4,1);
     }
-    echo "</table>";
   } else {
     echo "0 results";
   }
