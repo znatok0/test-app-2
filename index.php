8595368cc0295e5d746ed92502a645ba76f49a80
arnurkelgenbaev <galiaskar.uli@gmail.com>
2020-07-15 17:16:54 +0600

    soas

--- index.php
+++ index.php
@@ -22,103 +22,15 @@
         </header>
         <div class="container">
             <div class="test">
-            <style>
-                body {font-family: Arial, Helvetica, sans-serif;}
-
-                /* The Modal (background) */
-                .modal {
-                display: none; /* Hidden by default */
-                position: fixed; /* Stay in place */
-                z-index: 1; /* Sit on top */
-                padding-top: 100px; /* Location of the box */
-                left: 0;
-                top: 0;
-                width: 100%; /* Full width */
-                height: 100%; /* Full height */
-                overflow: auto; /* Enable scroll if needed */
-                background-color: rgb(0,0,0); /* Fallback color */
-                background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-                }
-
-                /* Modal Content */
-                .modal-content {
-                position: relative;
-                background-color: #fefefe;
-                margin: auto;
-                padding: 0;
-                border: 1px solid #888;
-                width: 80%;
-                box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
-                -webkit-animation-name: animatetop;
-                -webkit-animation-duration: 0.4s;
-                animation-name: animatetop;
-                animation-duration: 0.4s
-                }
-
-                /* Add Animation */
-                @-webkit-keyframes animatetop {
-                from {top:-300px; opacity:0} 
-                to {top:0; opacity:1}
-                }
-
-                @keyframes animatetop {
-                from {top:-300px; opacity:0}
-                to {top:0; opacity:1}
-                }
-
-                /* The Close Button */
-                .close {
-                color: white;
-                float: right;
-                font-size: 28px;
-                font-weight: bold;
-                }
-
-                .close:hover,
-                .close:focus {
-                color: #000;
-                text-decoration: none;
-                cursor: pointer;
-                }
-
-                .modal-header {
-                padding: 2px 16px;
-                background-color: #5cb85c;
-                color: white;
-                }
-
-                .modal-body {padding: 2px 16px;}
-
-                .modal-footer {
-                padding: 2px 16px;
-                background-color: #5cb85c;
-                color: white;
-                }
-                </style>
                 <center><form action="index.php" method="post">
                     <?php include('errors.php'); ?>       
-                    <div id="myModal" class="modal">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                            <span class="close">&times;</span>
-                            <h2>Modal Header</h2>
-                            </div>
-                            <div class="modal-body">
-                            <p>Some text in the Modal Body</p>
-                            <p>Some other text...</p>
-                            </div>
-                            <div class="modal-footer">
-                            <h3>Modal Footer</h3>
-                            </div>
-                        </div>
-                    </div>
                     <td>ФИО</td>
                     <td><input type='text' name='full_name' /></td>
                     <td>email</td>
                     <td><input type='text' name='email' /></td>
                     <td>ИИН</td>
                     <td><input type='text' name='iin' /></td>
-                    <td><a href="login.php">Войти</a></td>
+                    <!-- <td><a href="login.php">Войти</a></td> -->
                     <table border="1">
                         <tr><td align="center">N вопроса / Сұрақ N</td><td colspan="2" align="center">Варианты ответов / Жауап нұсқалары</td></tr>
                         <tr><td colspan="3" align="center" class="oddTableRow">Предположим, что после соответствующего обучения вы сможете выполнять любую работу. Однако если бы вам пришлось выбрать только из двух возможностей, что бы вы предпочли?
@@ -305,22 +217,6 @@
                         </tr>
                         <tr><td colspan="3" align="center"><button type="submit" class="btn" id="myBtn" name="insert_result">Отправить</button></td></tr>
                     </table>
-                    <script>
-                                var modal = document.getElementById('myModal');
-                                var btn = document.getElementById('myBtn');
-                                var span = document.getElementsByClassName('close')[0];
-                                btn.onclick = function() {
-                                modal.style.display = 'block';
-                                }
-                                span.onclick = function() {
-                                modal.style.display = 'none';
-                                }
-                                window.onclick = function(event) {
-                                if (event.target == modal) {
-                                    modal.style.display = 'none';
-                                }
-                                }
-                    </script>
                 </form></center>
                 </body>
                 </html>
--- select.php
+++ select.php
@@ -13,16 +13,6 @@ $sql = "SELECT * FROM result";
 $result = $connection->query($sql);
 
 if ($result->num_rows > 0) {
-    // output data of each row
-    // echo "<table>";
-    // echo"<tr><td>ИИН</td><td>Имя абитуриента</td><td>Email</td><td>Человека-Природа</td><td>Человек-Человек</td>";
-    // echo"<td>Человек-художественный образ</td><td>Человек-знаковая техника</td><td>Человек-техника</td></tr>";
-    // while($row = $result->fetch_assoc()) {
-    //     echo "<tr><td>".$row["iin"]."</td>"." "."<td>".$row["full_name"]." ".$row["email"]." ".
-    //     substr($row["result"],0,1)." ".substr($row["result"],1,1)." ".substr($row["result"],2,1)." ".
-    //     substr($row["result"],3,1)." ".substr($row["result"],4,1)."</td></tr>";
-    // }
-    // echo "</table>";
     echo "<p>Список абитуриентов, сдавших тест</p>";
     while($row = $result->fetch_assoc()) {
         echo "<p>ИИН:".$row["iin"]." "."ФИО:".$row["full_name"]." "."Человека-Природа:".substr($row["result"],0,1)." ".
