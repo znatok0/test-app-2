63d972cd98f4b0e87b46f22bb35473e7437ff385
arnurkelgenbaev <galiaskar.uli@gmail.com>
2020-07-15 17:56:35 +0600

    asdsad

--- index.php
+++ index.php
@@ -217,6 +217,49 @@
                         </tr>
                         <tr><td colspan="3" align="center"><button type="submit" class="btn" id="myBtn" name="insert_result" style="font-size:15px;">Отправить</button></td></tr>
                     </table>
+                    <div id="myModal" class="modal">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                            <span class="close">&times;</span>
+                            <h2>Modal Header</h2>
+                            </div>
+                            <div class="modal-body">
+                            <p>Some text in the Modal Body</p>
+                            <p>Some other text...</p>
+                            </div>
+                            <div class="modal-footer">
+                            <h3>Modal Footer</h3>
+                            </div>
+                        </div>
+                    </div>
+                    <button id="myBtn">Open Modal</button>
+                    <script>
+                        // Get the modal
+                        var modal = document.getElementById("myModal");
+
+                        // Get the button that opens the modal
+                        var btn = document.getElementById("myBtn");
+
+                        // Get the <span> element that closes the modal
+                        var span = document.getElementsByClassName("close")[0];
+
+                        // When the user clicks the button, open the modal 
+                        btn.onclick = function() {
+                        modal.style.display = "block";
+                        }
+
+                        // When the user clicks on <span> (x), close the modal
+                        span.onclick = function() {
+                        modal.style.display = "none";
+                        }
+
+                        // When the user clicks anywhere outside of the modal, close it
+                        window.onclick = function(event) {
+                        if (event.target == modal) {
+                            modal.style.display = "none";
+                        }
+                        }
+                    </script>
                 </form></center>
                 </body>
                 </html>
