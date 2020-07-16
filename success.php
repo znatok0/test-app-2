554215b5b5862bef476b2d14905f755156dabcec
arnurkelgenbaev <galiaskar.uli@gmail.com>
2020-07-16 17:50:43 +0600

    asddas

--- success.php
+++ success.php
@@ -9,8 +9,19 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
         <script type="text/javascript" src="main.js"></script>
+        <style>
+            div.success-form{
+                margin-top:10%;
+                margin:0 auto;
+            }
+            p.success-p{
+                font-size:20px;
+            }
+        </style>
     </head>
     <body>
-        <p>Ваш ответ успешно сохранен!</p>
+        <div class="success-form">
+            <p class="success-p">Ваш ответ успешно сохранен!</p>
+        </div>
     </body>
 </html>
