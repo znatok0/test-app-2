48bc134951a40941cf78f774569cfe480ab2a3d2
arnurkelgenbaev <galiaskar.uli@gmail.com>
2020-07-22 16:24:50 +0600

    aasdas

--- analytics.php
+++ analytics.php
@@ -10,7 +10,10 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
         <script type="text/javascript" src="main.js"></script>
     </head>
-    <script type='text/javascript'>
+</html>
+<?php include('server.php'); ?>
+<?php include('select.php'); ?> 
+<script type='text/javascript'>
       $(document).ready(function() {
       jQuery('#Export to excel').bind('click', function() {
       var target = $(this).attr('id');
@@ -24,7 +27,4 @@
       }
       });
           });
-    </script>
-</html>
-<?php include('server.php'); ?>
-<?php include('select.php'); ?> 
+</script>
