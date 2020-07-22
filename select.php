97cf00a3d1000722a3c46bc7f91f286691c378d6
arnurkelgenbaev <galiaskar.uli@gmail.com>
2020-07-22 16:34:26 +0600

    dada

--- select.php
+++ select.php
@@ -25,11 +25,11 @@ $result = $connection->query($sql);
       echo "<td>ID:".$row["id"]."</td>";
       echo "<td>ИИН:".$row["iin"]."</td>";
       echo "<td>ФИО:".$row["full_name"]."</td>";
-      echo "<td>Человека-Природа:".substr($row["result"],0,1)."</td>";
-      echo "<td>Человека-Человек:".substr($row["result"],1,1)."</td>";
-      echo "<td>Человека-художественный образ:".substr($row["result"],2,1)."</td>";
-      echo "<td>Человека-знаковая техника:".substr($row["result"],3,1)."</td>";
-      echo "<td>Человека-техника:".substr($row["result"],4,1)."</td>";
+      echo "<td>Человек-Природа:".substr($row["result"],0,1)."</td>";
+      echo "<td>Человек-Человек:".substr($row["result"],1,1)."</td>";
+      echo "<td>Человек-художественный образ:".substr($row["result"],2,1)."</td>";
+      echo "<td>Человек-знаковая техника:".substr($row["result"],3,1)."</td>";
+      echo "<td>Человек-техника:".substr($row["result"],4,1)."</td>";
       echo "<td>ИКТ:".$row["ikt"]."</td>";
       echo "</tr>";
     }
