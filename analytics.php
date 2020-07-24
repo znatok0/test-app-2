a156303d173ab4c5d9515634441f2a91542d8c20
arnurkelgenbaev <galiaskar.uli@gmail.com>
2020-07-24 15:15:47 +0600

    adsds

--- analytics.php
+++ analytics.php
@@ -29,26 +29,39 @@
             <input type='submit' name='export' value='CSV Export'>
         </form>
         <?php
-            if ($result->num_rows > 0) {
-                echo "<center><p style='font-size:20px;'>Список абитуриентов, сдавших тест</p></center>";
-                echo "<table style='margin:0 auto;'>";
-                while($row = $result->fetch_assoc()){
-                echo "<tr>";
-                echo "<td>ID:".$row["id"]."</td>";
-                echo "<td>ИИН:".$row["iin"]."</td>";
-                echo "<td>ФИО:".$row["full_name"]."</td>";
-                echo "<td>Человек-Природа:".substr($row["result"],0,1)."</td>";
-                echo "<td>Человек-Человек:".substr($row["result"],1,1)."</td>";
-                echo "<td>Человек-художественный образ:".substr($row["result"],2,1)."</td>";
-                echo "<td>Человек-знаковая система:".substr($row["result"],3,1)."</td>";
-                echo "<td>Человек-техника:".substr($row["result"],4,1)."</td>";
-                echo "<td>ИКТ:".$row["ikt"]."</td>";
-                echo "</tr>";
-                }
-                echo "</table>";
-            } else {
-                echo "0 results";
-            }
+            // if ($result->num_rows > 0) {
+            //     echo "<center><p style='font-size:20px;'>Список абитуриентов, сдавших тест</p></center>";
+            //     echo "<table style='margin:0 auto;'>";
+            //     while($row = $result->fetch_assoc()){
+            //     echo "<tr>";
+            //     echo "<td>ID:".$row["id"]."</td>";
+            //     echo "<td>ИИН:".$row["iin"]."</td>";
+            //     echo "<td>ФИО:".$row["full_name"]."</td>";
+            //     echo "<td>Человек-Природа:".substr($row["result"],0,1)."</td>";
+            //     echo "<td>Человек-Человек:".substr($row["result"],1,1)."</td>";
+            //     echo "<td>Человек-художественный образ:".substr($row["result"],2,1)."</td>";
+            //     echo "<td>Человек-знаковая система:".substr($row["result"],3,1)."</td>";
+            //     echo "<td>Человек-техника:".substr($row["result"],4,1)."</td>";
+            //     echo "<td>ИКТ:".$row["ikt"]."</td>";
+            //     echo "</tr>";
+            //     }
+            //     echo "</table>";
+            // } else {
+            //     echo "0 results";
+            // }
+            while($row = mysqli_fetch_array($result)){
         ?>
+        <tr>
+            <td><?php echo $row["id"]; ?></td>
+            <td><?php echo $row["iin"]; ?></td>
+            <td><?php echo $row["full_name"]; ?></td>
+            <td><?php echo substr($row["result"],0,1); ?></td>
+            <td><?php echo substr($row["result"],1,1); ?></td>
+            <td><?php echo substr($row["result"],2,1); ?></td>
+            <td><?php echo substr($row["result"],3,1); ?></td>
+            <td><?php echo substr($row["result"],4,1); ?></td>
+            <td><?php echo $row["ikt"]; ?></td>
+        </tr>
+        <?php } ?>
     </body>
 </html>
--- excel.php
+++ excel.php
@@ -1,5 +1,4 @@
 <?php 
-    die('hello!');
 if(isset($_POST["excel"])){
     $servername = "srv-pleskdb25.ps.kz:3306";
     $username = "kaznumil_root";
