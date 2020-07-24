4bcba279fd73624d792ac8d7f9a3f8ef18f244b0
arnurkelgenbaev <galiaskar.uli@gmail.com>
2020-07-24 16:57:14 +0600

    dadas

--- analytics.php
+++ analytics.php
@@ -26,7 +26,7 @@
     </head>
     <body>    
         <form method='post' action='excel.php'>
-            <input type='submit' name='export' value='Экспортировать в Excel'>
+            <input type='submit' name='excel' value='Экспортировать в Excel' />
         </form>
         <table style="margin:0 auto;">
             <tr>
